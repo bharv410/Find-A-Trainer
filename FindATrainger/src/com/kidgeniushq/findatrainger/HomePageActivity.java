cfcaaa183e8fd16875af48c13a947217b0593215
bharv410 <bharv14@gmail.com>
2014-11-15 19:21:44 -0800

    Cleaned up the code

--- FindATrainger/src/com/kidgeniushq/findatrainger/HomePageActivity.java
+++ FindATrainger/src/com/kidgeniushq/findatrainger/HomePageActivity.java
@@ -1,18 +1,10 @@
 package com.kidgeniushq.findatrainger;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -27,27 +19,11 @@ public class HomePageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         BugSenseHandler.initAndStartSession(getApplicationContext(), "64fbe08c");
-        
-        if(retrieveStatus().contains("trainer")){//if they are a trainer
-        	Intent i=new Intent(this,FindATrainerMainActivity.class);
-        	i.putExtra("occ", "trainer");
-	    	startActivity(i);
-        }
-        else if(retrieveStatus().contains("trainee")){// if they NEED a trainer
-        	Intent i=new Intent(this,FindATrainerMainActivity.class);
-        	i.putExtra("occ", "trainee");
-	    	startActivity(i);
-        }else if((retrieveStatus()!=null)&&(!retrieveStatus().equals(""))){ // if they started trainer signup
-        	Intent i=new Intent(this,ChooseVideoActivity.class);
-        	i.putExtra("name", retrieveStatus());
-	    	startActivity(i);
-        }else{
-        
-        	 requestWindowFeature(Window.FEATURE_NO_TITLE);
+                
+        	 requestWindowFeature(Window.FEATURE_NO_TITLE); //if first time
              getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
                                              WindowManager.LayoutParams.FLAG_FULLSCREEN); 
              setContentView(R.layout.activity_home_page);
-        }
      }
     public void needsATrainer(View v){    	
     	final Button button = (Button)findViewById(R.id.needsATrainerButton);
@@ -92,44 +68,6 @@ public class HomePageActivity extends Activity {
 			}
     		
     	}, 500);
-    	
-    }
-    private String retrieveStatus() {
-	    String ret = "";
-	    try {
-	        InputStream inputStream = openFileInput("trainerortrainee.txt");
-
-	        if ( inputStream != null ) {
-	            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-	            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-	            String receiveString = "";
-	            StringBuilder stringBuilder = new StringBuilder();
-
-	            while ( (receiveString = bufferedReader.readLine()) != null ) {
-	                stringBuilder.append(receiveString);
-	            }
-
-	            inputStream.close();
-	            ret = stringBuilder.toString();
-	        }
-	    }
-	    catch (FileNotFoundException e) {
-	        Log.e("login activity", "File not found: " + e.toString());
-	    } catch (IOException e) {
-	        Log.e("login activity", "Can not read file: " + e.toString());
-	    }
-
-	    return ret;
-	}
-    
-    
-    private class GetProfileInfoTask extends AsyncTask<String,Void,Void>{
-
-		@Override
-		protected Void doInBackground(String... arg0) {
-			return null;
-		}
-    	
     	
     }
 }
--- FindATrainger/src/com/kidgeniushq/findatrainger/SplashScreenActivity.java
+++ FindATrainger/src/com/kidgeniushq/findatrainger/SplashScreenActivity.java
@@ -1,10 +1,17 @@
 package com.kidgeniushq.findatrainger;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.bugsense.trace.BugSenseHandler;
 import com.kidgeniushq.findatrainger.helpers.AccountUtils;
@@ -18,55 +25,105 @@ public class SplashScreenActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		BugSenseHandler.initAndStartSession(getApplicationContext(), "64fbe08c");
+		BugSenseHandler
+				.initAndStartSession(getApplicationContext(), "64fbe08c");
 		setContentView(R.layout.activity_splash_screen);
 
-		//get lat long
-		LocationResult locationResult = new LocationResult(){
-		    @Override
-		    public void gotLocation(Location location){ 
-		    	if(location==null){
-		    		StaticVariables.lat=39;
-	    		StaticVariables.lng=76.7;
-		    	}else{
-		    		StaticVariables.lat=location.getLatitude();
-		    		StaticVariables.lng=location.getLongitude();
-		    	}		    	
-		    }
-		};	
-		//get username
-		String username=StaticVariables.retrieveUsername(this);
-		StaticVariables.username=username;
-		
-		//try to fill if not there
+		// get lat long
+		LocationResult locationResult = new LocationResult() {
+			@Override
+			public void gotLocation(Location location) {
+				if (location == null) {
+					StaticVariables.lat = 39;
+					StaticVariables.lng = 76.7;
+				} else {
+					StaticVariables.lat = location.getLatitude();
+					StaticVariables.lng = location.getLongitude();
+				}
+			}
+		};
+		// get username from FILE. (only will work if previously saved)
+		String username = StaticVariables.retrieveUsername(this);
+		StaticVariables.username = username;
+
+		// try to fill if not there (searches device ID NAME)
 		GetNameTask gnt = new GetNameTask();
 		gnt.execute();
 
-		//call the find location method that is declared above 
+		// call the find location method that is declared at the top
 		MyLocation myLocation = new MyLocation();
-		if(myLocation.getLocation(this, locationResult)){
-			startActivity(new Intent(this,HomePageActivity.class));
+		if (myLocation.getLocation(this, locationResult)) {
+			goToCorrectActivity();
+		}else{
+			StaticVariables.lat = 39;
+			StaticVariables.lng = 76.7;
+			goToCorrectActivity();
 		}
-		
+	}
+	private void goToCorrectActivity(){
+		if(retrieveStatus().contains("trainer")){//if they are a trainer
+        	Intent i=new Intent(this,FindATrainerMainActivity.class);
+        	i.putExtra("occ", "trainer");
+	    	startActivity(i);
+        }
+        else if(retrieveStatus().contains("trainee")){// if they NEED a trainer
+        	Intent i=new Intent(this,FindATrainerMainActivity.class);
+        	i.putExtra("occ", "trainee");
+	    	startActivity(i);
+        }else if((retrieveStatus()!=null)&&(!retrieveStatus().equals(""))){ // if they started trainer signup
+          	Intent i=new Intent(this,ChooseVideoActivity.class);               //but didnt finish
+        	i.putExtra("name", retrieveStatus());
+	    	startActivity(i);
+        }else{
+        	startActivity(new Intent(this, HomePageActivity.class));
+			finish();
+        }
+	}
+	private String retrieveStatus() {
+	    String ret = "";
+	    try {
+	        InputStream inputStream = openFileInput("trainerortrainee.txt");
+
+	        if ( inputStream != null ) {
+	            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+	            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+	            String receiveString = "";
+	            StringBuilder stringBuilder = new StringBuilder();
+
+	            while ( (receiveString = bufferedReader.readLine()) != null ) {
+	                stringBuilder.append(receiveString);
+	            }
+
+	            inputStream.close();
+	            ret = stringBuilder.toString();
+	        }
+	    }
+	    catch (FileNotFoundException e) {
+	        Log.e("login activity", "File not found: " + e.toString());
+	    } catch (IOException e) {
+	        Log.e("login activity", "Can not read file: " + e.toString());
+	    }
+
+	    return ret;
 	}
 	
-	private class GetNameTask extends AsyncTask<Void,Void,Void>{
+	
+	
+	
+	private class GetNameTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void... params) {
-			try{
-				//set hint for nameEditText using phone AccountUtils data
-				UserProfile possibleUserProfile=AccountUtils.getUserProfile(getApplicationContext());
-					StaticVariables.guessName=possibleUserProfile.possibleNames().get(0);
-					}catch(Exception e){
-						//if unavailable then set it to blank
-						StaticVariables.guessName="";
-					e.printStackTrace();
-				}
+			try {
+				// set hint for nameEditText using phone AccountUtils data
+				UserProfile possibleUserProfile = AccountUtils
+						.getUserProfile(getApplicationContext());
+				StaticVariables.guessName = possibleUserProfile.possibleNames()
+						.get(0);
+			} catch (Exception e) {
+				StaticVariables.guessName = "";
+				e.printStackTrace();
+			}
 			return null;
-		}
-		@Override
-		protected void onPostExecute(Void nun){
-			startActivity(new Intent(SplashScreenActivity.this,HomePageActivity.class));
 		}
 	}
 }
